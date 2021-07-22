53fde725eacb8c95fef4950a704fdcbc10cdd19d
Diego Jefferson <diegojfsr@gmail.com>
2021-07-22 17:25:54 -0300

    Primeiro codigo

---
+++ Primeiro codigo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Primeiros_passos_com_.NET___C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int numerodevezes = 5;
+
+           for (int i=0; i < numerodevezes; i++)
+           {
+               Console.WriteLine($"Bem vindo ao curso de .NET{i}");
+           }
+        }
+    }
+}
